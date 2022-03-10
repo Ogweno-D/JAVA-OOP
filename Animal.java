3065c47eb7ae9cc4b9b6dd391b73e945648baa0b
Ogweno <84031459+Ogweno-D@users.noreply.github.com>
2022-03-10 17:44:32 +0300

    Delete Animal.java

--- Animal.java
+++
@@ -1,9 +0,0 @@
-package MethodOveroading;
-
-public class Animal {
-
-    public void animalSound (String Sound){
-        System.out.println("The animal makes this specific sound." + Sound);
-    }
-    
-}
