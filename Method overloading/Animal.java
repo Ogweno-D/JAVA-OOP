71c5e853975b8f1470d7df07242ec1bede161da1
Ogweno <84031459+Ogweno-D@users.noreply.github.com>
2022-03-10 17:58:32 +0300

    Add files via upload
    This is a series of code to illustrate method overloading.

---
+++ Method overloading/Animal.java
@@ -0,0 +1,9 @@
+package MethodOveroading;
+
+public class Animal {
+
+    public void animalSound (String Sound){
+        System.out.println("The animal makes this specific sound." + Sound);
+    }
+    
+}
