cdbed80e70438df643f3bcf2d08354a8578c4ebf
Ogweno <84031459+Ogweno-D@users.noreply.github.com>
2022-06-20 11:22:02 +0300

    Update Animal.java

--- Method overloading/Animal.java
+++ Method overloading/Animal.java
@@ -3,7 +3,9 @@ package MethodOveroading;
 public class Animal {
 
     public void animalSound (String Sound){
+        String name;
         System.out.println("The animal makes this specific sound." + Sound);
+        System.out.printlm("This is a " + name )
     }
     
 }
